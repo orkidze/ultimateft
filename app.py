7fb0b1db2fd3265bdd0568768d1eb0725302e5d6
orkidze <hosvirinavicius@gmail.com>
2017-11-21 18:31:03 +0100

    S

--- app.py
+++ app.py
@@ -152,6 +152,10 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route("/google2b7fb394be91cf80")
+def google():
+    return render_template('google2b7fb394be91cf80.html')
+
 
 @app.route("/top50")
 @login_required
