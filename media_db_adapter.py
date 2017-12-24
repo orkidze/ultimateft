0672c7b454fe5648eee1c34874715ae03e0cdd7a
orkidze <hosvirinavicius@gmail.com>
2017-12-24 10:24:20 +0100

    S

--- media_db_adapter.py
+++ media_db_adapter.py
@@ -22,3 +22,11 @@ class media_db_adapter:
     def getTop5(self,id):
         for i in self.con.execute('select t.id, t.title, t.picture, t.content, t.date, u.username, t.date from website.top5 t inner join website.users u on t.author = u.id where t.id = '+id):
             return i
+
+    def nextTop5(self,id):
+        for i in self.con.execute('select id,title,picture from website.top5 where id > ' + id +' order by id ASC limit 1'):
+            return i
+    def preveusTop5(self,id):
+        for i in self.con.execute(
+                                'select id,title,picture from website.top5 where id < ' + id + ' order by id DESC limit 1'):
+            return i
