66514a796e45138ced6774c883c1f7dd7a8ce658
orkidze <hosvirinavicius@gmail.com>
2018-03-11 18:00:42 +0100

    Hello

--- media_db_adapter.py
+++ media_db_adapter.py
@@ -2,11 +2,11 @@ import sqlalchemy
 import os
 
 class media_db_adapter:
-    user = "jlemszjdrzdpcp"
-    password = "5535447b7c379896937f4111f3d37b2bd8f86da4b9d9e0c84d962779652ef455"
-    db = "d7d6vdurd0b8vu"
-    host = 'ec2-50-19-251-149.compute-1.amazonaws.com'
-    port = 5432
+    user = ""
+    password = ""
+    db = ""
+    host = ''
+    port = 0
 
     def __init__(self):
         url = 'postgresql://{}:{}@{}:{}/{}'
